69812c420b30eedb9b847a788952e6fd0b20456f
MatteoFar <matteo.farigu@gmail.com>
2022-09-28 12:51:32 +0200

    adding tryCatch and changing data sent to front on token

--- router.js
+++ router.js
@@ -7,9 +7,10 @@ const UsersModel = require("./models/users");
 const router = express.Router();
 
 router.get("/testDB", testDB);
-router.post("/signup", signUp);
+router.post("/signup", addUser);
+router.post("/signin", login);
 
-function signUp(req, res) {
+function addUser(req, res) {
   let newUser = { ...req.body };
   UsersModel.find().then((user) => {
     if (
@@ -35,6 +36,47 @@ function signUp(req, res) {
         res.status(200).send("Donnée envoyée avec succèes.");
       });
   });
+}
+
+function login(req, res) {
+  try {
+    let userLogin = { ...req.body };
+    UsersModel.findOne({ email: userLogin.email }).then((user) => {
+      if (!user) {
+        console.log("account Not found !");
+        return res
+          .status(404)
+          .send({ type: "login", message: "Compte introuvable" });
+      }
+      console.log("account found !");
+      bcrypt.compare(userLogin.password, user.password).then((isValid) => {
+        console.log(isValid);
+        if (isValid) {
+          const token = jwt.sign(
+            {
+              firstname: user.firstname,
+              lastname: user.lastname,
+            },
+            process.env.SECRET_API_TOKEN
+          );
+          return res.status(200).send({
+            type: "success",
+            message: "Connexion Validée.",
+            token: token,
+            user: user,
+          });
+        } else {
+          return res.status(403).send("mot de passe incorrect");
+        }
+      });
+
+      // res.status(200).send("Compte trouvé ! Demande de connexion reçu.");
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ type: "server", message: `Erreur serveur ${error}` });
+  }
 }
 
 function testDB(req, res) {
